6ff26019e35122fd0956678884a2db1eab327710
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-24 18:53:32 -0300

    Main package and main class.

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  
+  /*
+    args[1] = Name of the unique file, or, 
+              main folder which contains all of the other files.
+  */
+  public static void main(String args[]){
+    System.out.println(args[1]);
+  }
+}
