9cc6635b920fa9d7aaa7a75447e8839a90e9f963
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-24 22:34:23 -0300

    Testing the reader.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,26 @@
+package main;
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main{
   
   /*
-    args[1] = Name of the unique file, or, 
+    args[0] = Name of the unique file, or, 
               main folder which contains all of the other files.
   */
-  public static void main(String args[]){
-    System.out.println(args[1]);
+  public static void main(String args[]){      
+    try{      
+      WordReader wr = new WordReader(new File(args[0]));      
+      for(List<String> w = wr.nextWords(); w != null; w = wr.nextWords()){
+        for(String wi : w){
+          System.out.println(wi);
+        }
+      }
+    }
+    catch(FileNotFoundException notFound){
+      System.out.println("File was not found!");
+    }
   }
 }
