7ab84e23156e10fd8c3f906e80902a1e4bf9b222
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-25 09:58:09 -0300

    Taking the scanner dependency off.

--- src/main/WordReader.java
+++ src/main/WordReader.java
@@ -6,19 +6,13 @@ import java.util.ArrayList;
 import java.io.*;
 
 class WordReader{
-  private final int TOTAL_NUMBER_OF_WORDS = 4096;
-  
-  private Scanner s;  
-  
-  public WordReader(File f) throws FileNotFoundException{
-    this.s = new Scanner(f).useDelimiter("[,.;! $]+");
-  }
+  private static final int TOTAL_NUMBER_OF_WORDS = 4096;
   
   /*
     Read more words with the s scanner, this method keeps reading more words,
     until the sum of the length of all words is less than TOTAL_NUMBER_OF_CHARS.
   */
-  List<String> nextWords(){
+  public static List<String> nextWords(Scanner s){
     String next;
     List<String> words = new ArrayList<>();    
     while(words.size() < this.TOTAL_NUMBER_OF_WORDS && s.hasNext())
