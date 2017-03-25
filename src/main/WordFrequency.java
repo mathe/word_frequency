5ca3d826bfee93b952d0c16e7b799420ca9ad1c1
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-25 18:05:34 -0300

    Correct indentation.

--- src/main/WordFrequency.java
+++ src/main/WordFrequency.java
@@ -17,27 +17,27 @@ class WordFrequency implements FileVisitor<Path>{
   
   @Override
   public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-      return FileVisitResult.CONTINUE;
+    return FileVisitResult.CONTINUE;
   }
   
   @Override
   public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-      return FileVisitResult.CONTINUE;
+    return FileVisitResult.CONTINUE;
   }
   
   @Override
-  public FileVisitResult visitFile(Path dir, BasicFileAttributes attrs) throws IOException {      
-      Scanner s = new Scanner(dir.toFile()).useDelimiter("[,.;! $ \\s /]+");      
-      for(List<String> words = wordReader.nextWords(s); words != null; words = wordReader.nextWords(s))
-        for(String w : words)
-          this.freqIncrem.incrementFrequency(w);      
-      s.close();
-      return FileVisitResult.CONTINUE;
+  public FileVisitResult visitFile(Path dir, BasicFileAttributes attrs) throws IOException {         
+    Scanner s = new Scanner(dir.toFile()).useDelimiter("[,.;! $ \\s /]+");      
+    for(List<String> words = wordReader.nextWords(s); words != null; words = wordReader.nextWords(s))
+      for(String w : words)
+        this.freqIncrem.incrementFrequency(w);      
+    s.close();
+    return FileVisitResult.CONTINUE;
   }
   
   @Override
   public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-      return FileVisitResult.CONTINUE;
+    return FileVisitResult.CONTINUE;
   }
   
   public List<Frequency> getCurrWordFrequencies() throws IOException{
