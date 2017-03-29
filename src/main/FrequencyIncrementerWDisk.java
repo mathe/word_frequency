f41d0d1b15f1dd3b5454e8278d64adf4ad94fcf6
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-29 10:30:34 -0300

    counting frequency using files.

---
+++ src/main/FrequencyIncrementerWDisk.java
@@ -0,0 +1,68 @@
+package main;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class FrequencyIncrementerWDisk extends FrequencyIncrementer{
+  /*
+    Auxiliary directory for file criation.
+  */
+  
+  
+  /*
+    File for the TEMP_DIR, where the frequency of the words will be stored.
+  */
+  private File freqFolder;  
+  
+  public FrequencyIncrementerWDisk(){
+    this.freqFolder = FileSystems.getDefault().getPath(TEMP_DIR).toFile();    
+  }
+      
+  /* Increments the frequency of the given word. */
+  @Override
+  public void incrementFrequency(String word){        
+    try{
+      File file;
+      Path path = FileSystems.getDefault().getPath(TEMP_DIR + word);
+      int freq = 0;    
+      if(Files.exists(path)){
+        file = path.toFile();
+        Scanner scanner = new Scanner(file);
+        freq = scanner.nextInt();        
+        scanner.close();
+      }    
+      else {
+        path = Files.createFile(path);
+        file = path.toFile();
+      }    
+      freq++;
+      Files.write(file.toPath(),String.valueOf(freq).getBytes());
+    }
+    catch(IOException ioException){ //cover this case later.      
+    }    
+  }
+  
+  /*
+    Return a list of frequency objects with a word and it's current frequency.
+  */
+  @Override
+  public List<Frequency> getCurrFrequencies(){        
+    try{
+      List<Frequency> freqs = new ArrayList<>();
+      File[] wordFiles = this.freqFolder.listFiles();
+      for(File wf : wordFiles){      
+        Path path = wf.toPath();
+        List<String> lines = Files.readAllLines(path);
+        Frequency f = new Frequency(wf.getName(), Integer.valueOf(lines.get(0)));
+        freqs.add(f);
+      }
+      return freqs;
+    }
+    catch(IOException ioException){ //cover this case later.      
+    }   
+    return null; 
+  }
+}
